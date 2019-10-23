1ca5710a4565198910a7b1ad3ffb7d85da2a4aba
Calum <43376276+Calum532@users.noreply.github.com>
2019-10-23 02:31:45 +0100

    Started work on the menu scenes

---
+++ Game Engines 2 Game/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menuUI;
+    public GameObject raceTypeUI;
+    public GameObject raceTrackUI;
+    public GameObject chooseRacerUI;
+    public GameObject settingsUI;
+
+    public void RaceTypeSelect()
+    {
+        raceTypeUI.SetActive(true);
+        menuUI.SetActive(false);
+    }
+
+    public void RaceTrackSelect()
+    {
+        raceTypeUI.SetActive(false);
+        raceTrackUI.SetActive(true);
+    }
+
+    public void RacerSelect()
+    {
+        menuUI.SetActive(false);
+        raceTypeUI.SetActive(false);
+        raceTrackUI.SetActive(false);
+        chooseRacerUI.SetActive(true);
+    }
+
+    public void DisplaySettings()
+    {
+        settingsUI.SetActive(true);
+        menuUI.SetActive(false);
+    }
+
+    public void Back()
+    {
+        menuUI.SetActive(true);
+        raceTypeUI.SetActive(false);
+        settingsUI.SetActive(false);
+        raceTrackUI.SetActive(false);
+        chooseRacerUI.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quiting Game...");
+        Application.Quit();
+    }
+}
