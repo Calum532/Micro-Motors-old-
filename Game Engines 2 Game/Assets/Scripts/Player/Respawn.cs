eb40419d19e836316fab7306606e0f69e04884d7
Calum Bain <43376276+Calum532@users.noreply.github.com>
2019-10-23 09:40:05 +0100

    Revert "Revert "Started work on the menu scenes""
    This reverts commit 3a76e60e2e4ac323fc0a2562961ea68bb894a0d2.

---
+++ Game Engines 2 Game/Assets/Scripts/Player/Respawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform player;
+    public Transform respawnPoint;
+    GameObject obj;
+    GameObject AI;
+    public GameObject explosion;
+
+    private void Awake()
+    {
+        obj = GameObject.FindGameObjectWithTag("Player");
+        AI = GameObject.FindGameObjectWithTag("AICar");
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Respawn - Respawning Player");
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            FindObjectOfType<AudioManager>().Play("Explosion");
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
---
+++ Game Engines 2 Game/Assets/Scripts/UI/LevelEnd.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+    public GameObject levelCompleteUI;
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        levelCompleteUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+        levelCompleteUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("LEVEL COMPLETE");
+            FindObjectOfType<AudioManager>().Play("EndGong");
+            levelCompleteUI.SetActive(true);
+            Time.timeScale = 0f;
+            PauseMenu.gameIsPaused = true;
+        }
+    }
+}
