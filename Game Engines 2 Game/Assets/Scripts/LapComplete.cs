70b8eb726bc4b337258999cb94e64b6463352888
Calum <43376276+Calum532@users.noreply.github.com>
2019-10-25 01:11:37 +0100

    Started work on in-game UI and Lap system

---
+++ Game Engines 2 Game/Assets/Scripts/LapComplete.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LapComplete : MonoBehaviour
+{
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    public GameObject BestMinuteDisplay;
+    public GameObject BestSecondDisplay;
+    public GameObject BestMilliDisplay;
+
+    public GameObject LapTimeBox;
+
+    void OnTriggerEnter()
+    {
+        if(RaceTimer.SecondCount <= 9)
+        {
+            BestSecondDisplay.GetComponent<TextMeshProUGUI>().text = "0" + RaceTimer.SecondCount + ".";
+        }
+        else
+        {
+            BestSecondDisplay.GetComponent<TextMeshProUGUI>().text = "" + RaceTimer.SecondCount + ".";
+        }
+
+        if (RaceTimer.MinuteCount <= 9)
+        {
+            BestMinuteDisplay.GetComponent<TextMeshProUGUI>().text = "0" + RaceTimer.MinuteCount + ".";
+        }
+        else
+        {
+            BestMinuteDisplay.GetComponent<TextMeshProUGUI>().text = "" + RaceTimer.MinuteCount + ".";
+        }
+
+        BestMilliDisplay.GetComponent<TextMeshProUGUI>().text = "" + RaceTimer.MilliCount.ToString("F0");
+
+        RaceTimer.MinuteCount = 0;
+        RaceTimer.SecondCount = 0;
+        RaceTimer.MilliCount = 0;
+
+        HalfLapTrig.SetActive(true);
+        LapCompleteTrig.SetActive(false);
+    }
+}
---
+++ Game Engines 2 Game/Assets/Scripts/RaceTimer.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityStandardAssets.Vehicles.Car;
+
+public class RaceTimer : MonoBehaviour
+{
+    private float countdownTimer = 3;
+    public static float cTimer;
+    private bool raceTimer = false;
+
+    public static float finishTime;
+    private bool finished;
+    public GameObject raceResultsUI;
+
+    public static int MinuteCount;
+    public static int SecondCount;
+    public static float MilliCount;
+    public static string MilliDisplay;
+
+    public GameObject MinuteBox;
+    public GameObject SecondBox;
+    public GameObject MilliBox;
+
+    bool raceStarted = false;
+    GameObject[] AICars;
+
+    private void Start()
+    {
+        cTimer = countdownTimer;
+
+        AICars = GameObject.FindGameObjectsWithTag("AICar");
+        foreach (GameObject car in AICars)
+        {
+            car.GetComponent<CarAIControl>().enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        //start 3 second race countdown
+        if (raceTimer == false)
+        {
+            PauseMenu.gameIsPaused = true;
+            cTimer -= Time.deltaTime;
+        }
+
+        //Race starts
+        if (cTimer <= 0f)
+        {
+            PauseMenu.gameIsPaused = false;
+            raceTimer = true;
+
+            raceStarted = true;
+        }
+
+        //Start lap timer
+        if (finished == false & raceTimer == true)
+        {
+            MilliCount += Time.deltaTime * 10;
+            MilliDisplay = MilliCount.ToString("F0");
+            MilliBox.GetComponent<TextMeshProUGUI>().text = "" + MilliDisplay;
+
+            if (MilliCount >= 9)
+            {
+                MilliCount = 0;
+                SecondCount += 1;
+            }
+
+            if (SecondCount <= 9)
+            {
+                SecondBox.GetComponent<TextMeshProUGUI>().text = "0" + SecondCount + ".";
+            } else
+            {
+                SecondBox.GetComponent<TextMeshProUGUI>().text = "" + SecondCount + ".";
+            }
+
+            if (SecondCount >= 60)
+            {
+                SecondCount = 0;
+                MinuteCount += 1;
+            }
+
+            if (MinuteCount <= 9)
+            {
+                MinuteBox.GetComponent<TextMeshProUGUI>().text = "0" + MinuteCount + ":";
+            } else
+            {
+                MinuteBox.GetComponent<TextMeshProUGUI>().text = "" + MinuteCount + ":";
+            }
+
+            if (raceStarted)
+            {
+                foreach (GameObject car in AICars)
+                {
+                    car.GetComponent<CarAIControl>().enabled = true;
+                }
+            }
+        }
+
+        //if player finishes race
+        /* if (Player finishes race)
+        {
+            finishTime = t;
+            finished = true;
+            timerText.color = Color.red;
+            raceResultsUI.SetActive(true);
+            PauseMenu.gameIsPaused = true;
+        } */
+    }
+}
