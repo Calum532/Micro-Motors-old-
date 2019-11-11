4b3870fbccfdc11b4fc0e86c2999336edf84232d
Calum <43376276+Calum532@users.noreply.github.com>
2019-11-11 02:59:22 +0000

    highlighting a racer now displays a boarder and vehicle stats, imported DOTween asset pack

--- Game Engines 2 Game/Assets/Scripts/HighlightRacer.cs
+++ Game Engines 2 Game/Assets/Scripts/HighlightRacer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HighlightRacer : MonoBehaviour
 {
@@ -14,7 +15,30 @@ public class HighlightRacer : MonoBehaviour
     public GameObject Dozer;
     public GameObject SkyCar;
     public GameObject Random;
+
+    public GameObject RCBorder;
+    public GameObject UFOBorder;
+    public GameObject TaxiBorder;
+    public GameObject TruckBorder;
+    public GameObject TankBorder;
+    public GameObject MonsterBorder;
+    public GameObject DozerBorder;
+    public GameObject SkyCarBorder;
+    public GameObject RandomBorder;
+
     public GameObject RacerNameUI;
+
+    public Slider TopSpeedSlider;
+    public Slider AccelerationSlider;
+    public Slider GripSlider;
+    public Slider HandlingSlider;
+    public Slider WeightSlider;
+
+    public Image TopSpeedFill;
+    public Image AccelerationFill;
+    public Image GripFill;
+    public Image HandlingFill;
+    public Image WeightFill;
 
     public void mouseOverRC()
     {
@@ -27,6 +51,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(true);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 1;
+        AccelerationSlider.value = 1;
+        GripSlider.value = 1;
+        HandlingSlider.value = 1;
+        WeightSlider.value = 1;
+
+        TopSpeedFill.color = new Color32(0, 0, 255, 255);
+        AccelerationFill.color = new Color32(0, 0, 255, 255);
+        GripFill.color = new Color32(0, 0, 255, 255);
+        HandlingFill.color = new Color32(0, 0, 255, 255);
+        WeightFill.color = new Color32(0, 0, 255, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "R.C.";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(0, 0, 255, 255);
@@ -43,9 +90,32 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(true);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 2;
+        AccelerationSlider.value = 2;
+        GripSlider.value = 2;
+        HandlingSlider.value = 2;
+        WeightSlider.value = 2;
+
+        TopSpeedFill.color = new Color32(169, 169, 169, 255);
+        AccelerationFill.color = new Color32(169, 169, 169, 255);
+        GripFill.color = new Color32(169, 169, 169, 255);
+        HandlingFill.color = new Color32(169, 169, 169, 255);
+        WeightFill.color = new Color32(169, 169, 169, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "UFO";
-        RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(125, 125, 125, 255);
+        RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(169, 169, 169, 255);
     }
 
     public void mouseOverTaxi()
@@ -59,6 +129,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(true);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 3;
+        AccelerationSlider.value = 3;
+        GripSlider.value = 3;
+        HandlingSlider.value = 3;
+        WeightSlider.value = 3;
+
+        TopSpeedFill.color = new Color32(255, 255, 0, 255);
+        AccelerationFill.color = new Color32(255, 255, 0, 255);
+        GripFill.color = new Color32(255, 255, 0, 255);
+        HandlingFill.color = new Color32(255, 255, 0, 255);
+        WeightFill.color = new Color32(255, 255, 0, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Taxi";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 0, 255);
@@ -75,6 +168,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(true);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 4;
+        AccelerationSlider.value = 4;
+        GripSlider.value = 4;
+        HandlingSlider.value = 4;
+        WeightSlider.value = 4;
+
+        TopSpeedFill.color = new Color32(255, 0, 0, 255);
+        AccelerationFill.color = new Color32(255, 0, 0, 255);
+        GripFill.color = new Color32(255, 0, 0, 255);
+        HandlingFill.color = new Color32(255, 0, 0, 255);
+        WeightFill.color = new Color32(255, 0, 0, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Truck";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(255, 0, 0, 255);
@@ -91,6 +207,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(true);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 5;
+        AccelerationSlider.value = 5;
+        GripSlider.value = 5;
+        HandlingSlider.value = 5;
+        WeightSlider.value = 5;
+
+        TopSpeedFill.color = new Color32(0, 150, 0, 255);
+        AccelerationFill.color = new Color32(0, 150, 0, 255);
+        GripFill.color = new Color32(0, 150, 0, 255);
+        HandlingFill.color = new Color32(0, 150, 0, 255);
+        WeightFill.color = new Color32(0, 150, 0, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Tank";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(0, 150, 0, 255);
@@ -107,6 +246,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(true);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 6;
+        AccelerationSlider.value = 6;
+        GripSlider.value = 6;
+        HandlingSlider.value = 6;
+        WeightSlider.value = 6;
+
+        TopSpeedFill.color = new Color32(125, 0, 255, 255);
+        AccelerationFill.color = new Color32(125, 0, 255, 255);
+        GripFill.color = new Color32(125, 0, 255, 255);
+        HandlingFill.color = new Color32(125, 0, 255, 255);
+        WeightFill.color = new Color32(125, 0, 255, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Monster";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(125, 0, 255, 255);
@@ -123,6 +285,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(true);
         SkyCar.SetActive(false);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(true);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 7;
+        AccelerationSlider.value = 7;
+        GripSlider.value = 7;
+        HandlingSlider.value = 7;
+        WeightSlider.value = 7;
+
+        TopSpeedFill.color = new Color32(255, 125, 0, 255);
+        AccelerationFill.color = new Color32(255, 125, 0, 255);
+        GripFill.color = new Color32(255, 125, 0, 255);
+        HandlingFill.color = new Color32(255, 125, 0, 255);
+        WeightFill.color = new Color32(255, 125, 0, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Dozer";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(255, 125, 0, 255);
@@ -139,6 +324,29 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(true);
         Random.SetActive(false);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(true);
+        RandomBorder.SetActive(false);
+
+        TopSpeedSlider.value = 8;
+        AccelerationSlider.value = 8;
+        GripSlider.value = 8;
+        HandlingSlider.value = 8;
+        WeightSlider.value = 8;
+
+        TopSpeedFill.color = new Color32(0, 255, 255, 255);
+        AccelerationFill.color = new Color32(0, 255, 255, 255);
+        GripFill.color = new Color32(0, 255, 255, 255);
+        HandlingFill.color = new Color32(0, 255, 255, 255);
+        WeightFill.color = new Color32(0, 255, 255, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
         RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Sky Car";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(0, 255, 255, 255);
@@ -155,8 +363,31 @@ public class HighlightRacer : MonoBehaviour
         Dozer.SetActive(false);
         SkyCar.SetActive(false);
         Random.SetActive(true);
+
+        RCBorder.SetActive(false);
+        UFOBorder.SetActive(false);
+        TaxiBorder.SetActive(false);
+        TruckBorder.SetActive(false);
+        TankBorder.SetActive(false);
+        MonsterBorder.SetActive(false);
+        DozerBorder.SetActive(false);
+        SkyCarBorder.SetActive(false);
+        RandomBorder.SetActive(true);
+
+        TopSpeedSlider.value = 0;
+        AccelerationSlider.value = 0;
+        GripSlider.value = 0;
+        HandlingSlider.value = 0;
+        WeightSlider.value = 0;
+
+        TopSpeedFill.color = new Color32(255, 255, 255, 255);
+        AccelerationFill.color = new Color32(255, 255, 255, 255);
+        GripFill.color = new Color32(255, 255, 255, 255);
+        HandlingFill.color = new Color32(255, 255, 255, 255);
+        WeightFill.color = new Color32(255, 255, 255, 255);
+
         FindObjectOfType<AudioManager>().Play("Click");
-        RacerNameUI.GetComponent<TextMeshProUGUI>().text = "?????????";
+        RacerNameUI.GetComponent<TextMeshProUGUI>().text = "Random";
         RacerNameUI.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 255);
     }
 }
